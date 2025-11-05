efcf37d68b131758ab10ca2376b0a9ddde14c3cb
nadunyalegama <nadunyalegama@gmail.com>
2025-11-05 21:20:35 +0530

    created api requests

--- employee-backend/src/main/java/com/employee/employee/backend/controller/EmployeeController.java
+++ employee-backend/src/main/java/com/employee/employee/backend/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("http://localhost:3000/")
 @RequestMapping("api/v1/")
 public class EmployeeController {
 
